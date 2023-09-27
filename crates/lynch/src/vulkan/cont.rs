3a4ca375b2e337e8f13245cce5150043bae4f6a0
Aidan Dowling <me@aodhain.ie>
2023-09-27 15:20:49 +0100

    VkContext Created

---
+++ crates/lynch/src/vulkan/cont.rs
@@ -0,0 +1,56 @@
+use ash::{
+    extensions::{ext::DebugReport, khr::Surface},
+    version::{DeviceV1_0, InstanceV1_0},
+    vk, Device, Entry, Instance,
+};
+
+pub struct VkContext {
+    _entry: Entry,
+    instance: Instance,
+    debug_report_callback: Option<(DebugReport, vk::DebugReportCallbackEXT)>,
+    surface: Surface,
+    surface_khr: vk::SurfaceKHR,
+    physical_device: vk::PhysicalDevice,
+    device: Device,
+}
+
+impl VkContext {
+    pub fn new(
+        entry: Entry,
+        instance: Instance,
+        debug_report_callback: Option<(DebugReport, vk::DebugReportCallbackEXT)>,
+        surface: Surface,
+        surface_khr: vk::SurfaceKHR,
+        physical_device: vk::PhysicalDevice,
+        device: Device,
+    ) -> Self {
+        VkContext {
+            _entry: entry,
+            instance,
+            debug_report_callback,
+            surface,
+            surface_khr,
+            physical_device,
+            device,
+        }
+    }
+    pub fn instance(&self) -> &Instance {
+        &self.instance
+    }
+
+    pub fn surface(&self) -> &Surface {
+        &self.surface
+    }
+
+    pub fn surface_khr(&self) -> vk::SurfaceKHR {
+        self.surface_khr
+    }
+
+    pub fn physical_device(&self) -> vk::PhysicalDevice {
+        self.physical_device
+    }
+
+    pub fn device(&self) -> &Device {
+        &self.device
+    }
+}

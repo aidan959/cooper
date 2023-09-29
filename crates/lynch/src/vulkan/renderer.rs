e971cf449d5f049e79c08806bfe309382a7e1902
Aidan Dowling <me@aodhain.ie>
2023-09-29 11:08:17 +0100

    check device suitability

--- crates/lynch/src/vulkan/renderer.rs
+++ crates/lynch/src/vulkan/renderer.rs
@@ -81,6 +81,75 @@ impl VulkanRenderer {
         };
         (device, queue_families_indices)
     }
+    fn is_device_suitable(
+        instance: &Instance,
+        surface: &Surface,
+        surface_khr: vk::SurfaceKHR,
+        device: vk::PhysicalDevice,
+    ) -> bool {
+        let (graphics, present) = Self::find_queue_families(instance, surface, surface_khr, device);
+        let extention_support = Self::check_device_extension_support(instance, device);
+        let is_swapchain_adequate = {
+            let details = SwapchainSupportDetails::new(device, surface, surface_khr);
+            !details.formats.is_empty() && !details.present_modes.is_empty()
+        };
+        let features = unsafe { instance.get_physical_device_features(device) };
+        graphics.is_some()
+            && present.is_some()
+            && extention_support
+            && is_swapchain_adequate
+            && features.sampler_anisotropy == vk::TRUE
+    }
+    fn check_device_extension_support(instance: &Instance, device: vk::PhysicalDevice) -> bool {
+        let required_extentions = Self::get_required_device_extensions();
+
+        let extension_props = unsafe {
+            instance
+                .enumerate_device_extension_properties(device)
+                .unwrap()
+        };
+
+        for required in required_extentions.iter() {
+            let found = extension_props.iter().any(|ext| {
+                let name = unsafe { CStr::from_ptr(ext.extension_name.as_ptr()) };
+                required == &name
+            });
+
+            if !found {
+                return false;
+            }
+        }
+
+        true
+    }
+    fn find_queue_families(
+        instance: &Instance,
+        surface: &Surface,
+        surface_khr: vk::SurfaceKHR,
+        device: vk::PhysicalDevice,
+    ) -> (Option<u32>, Option<u32>) {
+        let (mut graphics, mut present) = (None, None);
+
+        let props = unsafe { instance.get_physical_device_queue_family_properties(device) };
+
+        for (index, family) in props.iter().filter(|f| f.queue_count > 0).enumerate() {
+            let index = index as u32;
+            if family.queue_flags.contains(vk::QueueFlags::GRAPHICS) && graphics.is_none() {
+                graphics = Some(index);
+            }
+            if unsafe { surface.get_physical_device_surface_support(device, index, surface_khr) }
+                && present.is_none()
+            {
+                present = Some(index);
+            }
+
+            if graphics.is_some() && present.is_some() {
+                break;
+            };
+        }
+
+        (graphics, present)
+    }
 }
 
 
@@ -100,11 +169,21 @@ impl Renderer for VulkanRenderer {
 
         let debug_report_callback = setup_debug_messenger(&entry, &instance);
 
-        let (physical_device, queue_family_indices) =
-            
+        let (physical_device, queue_family_indices) = 
+            Self::get_physical_device(&instance, &surface, surface_khr);
+
+        let vk_context = VkContext::new(
+            entry,
+            instance,
+            debug_report_callback,
+            surface,
+            surface_khr,
+            physical_device,
+            logical_device,
+        );
         Self {
             None,
-            todo!()
+            vk_context
         }
     }
 }

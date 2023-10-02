78dcded38934242c5f0e0d52353e17626c7f4cb3
Aidan Dowling <me@aodhain.ie>
2023-10-02 19:22:05 +0100

    finished method prototyping

--- crates/lynch/src/vulkan/renderer.rs
+++ crates/lynch/src/vulkan/renderer.rs
@@ -608,6 +608,22 @@ impl VulkanRenderer {
                 .unwrap()
         }
     }
+    fn find_memory_type(
+        requirements: vk::MemoryRequirements,
+        mem_properties: vk::PhysicalDeviceMemoryProperties,
+        required_properties: vk::MemoryPropertyFlags,
+    ) -> u32 {
+        for i in 0..mem_properties.memory_type_count {
+            if requirements.memory_type_bits & (1 << i) != 0
+                && mem_properties.memory_types[i as usize]
+                    .property_flags
+                    .contains(required_properties)
+            {
+                return i;
+            }
+        }
+        panic!("Failed to find suitable memory type.")
+    }
     fn create_image(
         vk_context: &VkContext,
         mem_properties: vk::MemoryPropertyFlags,

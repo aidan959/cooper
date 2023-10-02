57e9e0d0985d06d33627aced1598ca1b42736cc0
Aidan Dowling <me@aodhain.ie>
2023-10-02 19:54:13 +0100

    execute one time created

--- crates/lynch/src/vulkan/renderer.rs
+++ crates/lynch/src/vulkan/renderer.rs
@@ -624,6 +624,57 @@ impl VulkanRenderer {
         }
         panic!("Failed to find suitable memory type.")
     }
+        /// Create a one time use command buffer and pass it to `executor`.
+        fn execute_one_time_commands<F: FnOnce(vk::CommandBuffer)>(
+            device: &Device,
+            command_pool: vk::CommandPool,
+            queue: vk::Queue,
+            executor: F,
+        ) {
+            let command_buffer = {
+                let alloc_info = vk::CommandBufferAllocateInfo::builder()
+                    .level(vk::CommandBufferLevel::PRIMARY)
+                    .command_pool(command_pool)
+                    .command_buffer_count(1)
+                    .build();
+    
+                unsafe { device.allocate_command_buffers(&alloc_info).unwrap()[0] }
+            };
+            let command_buffers = [command_buffer];
+    
+            // Begin recording
+            {
+                let begin_info = vk::CommandBufferBeginInfo::builder()
+                    .flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT)
+                    .build();
+                unsafe {
+                    device
+                        .begin_command_buffer(command_buffer, &begin_info)
+                        .unwrap()
+                };
+            }
+            executor(command_buffer);
+    
+            // End recording
+            unsafe { device.end_command_buffer(command_buffer).unwrap() };
+    
+            // Submit and wait
+            {
+                let submit_info = vk::SubmitInfo::builder()
+                    .command_buffers(&command_buffers)
+                    .build();
+                let submit_infos = [submit_info];
+                unsafe {
+                    device
+                        .queue_submit(queue, &submit_infos, vk::Fence::null())
+                        .unwrap();
+                    device.queue_wait_idle(queue).unwrap();
+                };
+            }
+    
+            // Free
+            unsafe { device.free_command_buffers(command_pool, &command_buffers) };
+        }
     fn create_image(
         vk_context: &VkContext,
         mem_properties: vk::MemoryPropertyFlags,

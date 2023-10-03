efbf4ffde01f9d8c709f9aa3c60a842cc0fbe22e
Aidan Dowling <me@aodhain.ie>
2023-10-03 19:14:51 +0100

    descriptor set logic

--- crates/lynch/src/vulkan/renderer.rs
+++ crates/lynch/src/vulkan/renderer.rs
@@ -34,8 +34,13 @@ pub struct VulkanRenderer {
     depth_texture: Texture,
     color_texture:Texture,
     texture: Texture,
-    swapchain_framebuffers: Vec<vk::Framebuffer>
-
+    swapchain_framebuffers: Vec<vk::Framebuffer>,
+    uniform_buffers: Vec<vk::Buffer>,
+    uniform_buffer_memories: Vec<vk::DeviceMemory>,
+    command_buffers: Vec<vk::CommandBuffer>,
+    vertex_buffer_memory: vk::DeviceMemory,
+    descriptor_pool: vk::DescriptorPool,
+    descriptor_sets: Vec<vk::DescriptorSet>,
 }
 
 impl VulkanRenderer {
@@ -736,6 +741,81 @@ impl VulkanRenderer {
             logical_device.destroy_shader_module(fragment_shader_module, None);
         };
         (pipeline, pipeline_layout)
+    }
+    fn create_descriptor_pool(device: &Device, size: u32) -> vk::DescriptorPool {
+        let ubo_pool_size = vk::DescriptorPoolSize {
+            ty: vk::DescriptorType::UNIFORM_BUFFER,
+            descriptor_count: size,
+        };
+        let sampler_pool_size = vk::DescriptorPoolSize {
+            ty: vk::DescriptorType::COMBINED_IMAGE_SAMPLER,
+            descriptor_count: size,
+        };
+
+        let pool_sizes = [ubo_pool_size, sampler_pool_size];
+
+        let pool_info = vk::DescriptorPoolCreateInfo::builder()
+            .pool_sizes(&pool_sizes)
+            .max_sets(size)
+            .build();
+
+        unsafe { device.create_descriptor_pool(&pool_info, None).unwrap() }
+    }
+    /// Create one descriptor set for each uniform buffer.
+    fn create_descriptor_sets(
+        device: &Device,
+        pool: vk::DescriptorPool,
+        layout: vk::DescriptorSetLayout,
+        uniform_buffers: &[vk::Buffer],
+        texture: Texture,
+    ) -> Vec<vk::DescriptorSet> {
+        let layouts = (0..uniform_buffers.len())
+            .map(|_| layout)
+            .collect::<Vec<_>>();
+        let alloc_info = vk::DescriptorSetAllocateInfo::builder()
+            .descriptor_pool(pool)
+            .set_layouts(&layouts)
+            .build();
+        let descriptor_sets = unsafe { device.allocate_descriptor_sets(&alloc_info).unwrap() };
+
+        descriptor_sets
+            .iter()
+            .zip(uniform_buffers.iter())
+            .for_each(|(set, buffer)| {
+                let buffer_info = vk::DescriptorBufferInfo::builder()
+                    .buffer(*buffer)
+                    .offset(0)
+                    .range(std::mem::size_of::<UniformBufferObject>() as vk::DeviceSize)
+                    .build();
+                let buffer_infos = [buffer_info];
+
+                let image_info = vk::DescriptorImageInfo::builder()
+                    .image_layout(vk::ImageLayout::SHADER_READ_ONLY_OPTIMAL)
+                    .image_view(texture.view)
+                    .sampler(texture.sampler.unwrap())
+                    .build();
+                let image_infos = [image_info];
+
+                let ubo_descriptor_write = vk::WriteDescriptorSet::builder()
+                    .dst_set(*set)
+                    .dst_binding(0)
+                    .dst_array_element(0)
+                    .descriptor_type(vk::DescriptorType::UNIFORM_BUFFER)
+                    .buffer_info(&buffer_infos)
+                    .build();
+                let sampler_descriptor_write = vk::WriteDescriptorSet::builder()
+                    .dst_set(*set)
+                    .dst_binding(1)
+                    .dst_array_element(0)
+                    .descriptor_type(vk::DescriptorType::COMBINED_IMAGE_SAMPLER)
+                    .image_info(&image_infos)
+                    .build();
+
+                let descriptor_writes = [ubo_descriptor_write, sampler_descriptor_write];
+                unsafe { device.update_descriptor_sets(&descriptor_writes, &[]) }
+            });
+
+        descriptor_sets
     }
     fn create_command_pool(
         device: &Device,
@@ -1440,6 +1520,17 @@ impl Renderer for VulkanRenderer {
             transient_command_pool,
             graphics_queue,
             &indices,
+        );
+        let (uniform_buffers, uniform_buffer_memories) =
+            Self::create_uniform_buffers(&vk_context, images.len());
+
+        let descriptor_pool = Self::create_descriptor_pool(vk_context.device(), images.len() as _);
+        let descriptor_sets = Self::create_descriptor_sets(
+            vk_context.device(),
+            descriptor_pool,
+            descriptor_set_layout,
+            &uniform_buffers,
+            texture,
         );
         Self {
             resize_dimensions:None,

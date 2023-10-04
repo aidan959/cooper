844a158a78337200b3af4ccd155cd44993b986c6
Aidan Dowling <me@aodhain.ie>
2023-10-04 01:47:08 +0100

    swapchain code didnt commit correctly

--- crates/lynch/src/math.rs
+++ crates/lynch/src/math.rs
@@ -1,5 +1,41 @@
 use cgmath::prelude::*;
 use cgmath::{BaseFloat, Matrix4, Rad};
+pub fn perspective<S, F>(fovy: F, aspect: S, near: S, far: S) -> Matrix4<S>
+where
+    S: BaseFloat,
+    F: Into<Rad<S>>,
+{
+    let two = S::one() + S::one();
+    let f = Rad::cot(fovy.into() / two);
+
+    let c0r0 = f / aspect;
+    let c0r1 = S::zero();
+    let c0r2 = S::zero();
+    let c0r3 = S::zero();
+
+    let c1r0 = S::zero();
+    let c1r1 = -f;
+    let c1r2 = S::zero();
+    let c1r3 = S::zero();
+
+    let c2r0 = S::zero();
+    let c2r1 = S::zero();
+    let c2r2 = -far / (far - near);
+    let c2r3 = -S::one();
+
+    let c3r0 = S::zero();
+    let c3r1 = S::zero();
+    let c3r2 = -(far * near) / (far - near);
+    let c3r3 = S::zero();
+
+    #[cfg_attr(rustfmt, rustfmt::skip)]
+    Matrix4::new(
+        c0r0, c0r1, c0r2, c0r3,
+        c1r0, c1r1, c1r2, c1r3,
+        c2r0, c2r1, c2r2, c2r3,
+        c3r0, c3r1, c3r2, c3r3,
+    )
+}
 
 pub fn normalize(vector : [f32; 3]) -> [f32; 3]{
     let length = (vector[0].powf(2.) + vector[1].powf(2.) + vector[2].powf(2.)).sqrt();

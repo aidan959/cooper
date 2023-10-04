584671ac1f8699d4d72906015ca8d502021665bc
Aidan Dowling <me@aodhain.ie>
2023-10-04 12:46:07 +0100

    defs

--- crates/application/application.rs
+++ crates/application/application.rs
@@ -1,9 +1,19 @@
 
+
 enum GameEvent {
     InputEvent,
     UpdateEvent,
     RenderEvent
 }
+
+pub struct EventHandler {
+    input_subscribers: Vec<Box<dyn Fn(&GameEvent) -> ()>>,
+    update_subscribers: Vec<Box<dyn Fn(&GameEvent) -> ()>>, 
+    render_subscribers: Vec<Box<dyn Fn() -> ()>>,
+}
+
+
+
 impl EventHandler {
     fn new() -> Self {
         EventHandler {
@@ -38,35 +48,27 @@ impl EventHandler {
             .for_each(|subscriber: &Box<dyn Fn(&GameEvent)>| { subscriber(&event)});
     }
 
-    fn dispatch_render_event(&self, event: &GameEvent) {
+    fn dispatch_render_event(&self, _event: &GameEvent) {
         &self.render_subscribers
             .iter()
             .for_each(|subscriber: &Box<dyn Fn()>| { subscriber()});
     }
 }
-pub struct EventHandler {
-    input_subscribers: Vec<Box<dyn Fn(&GameEvent) -> ()>>,
-    update_subscribers: Vec<Box<dyn Fn(&GameEvent) -> ()>>, 
-    render_subscribers: Vec<Box<dyn Fn() -> ()>>,
-}
-
 use lynch::{window::window::Window, renderer::Renderer};
 use lynch::vulkan::renderer::VulkanRenderer;
-use winit::event_loop;
+
 use winit::{
     dpi::PhysicalSize,
-    event::{ElementState, Event, MouseButton, MouseScrollDelta, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::{WindowBuilder},
+    event::{Event, MouseButton, MouseScrollDelta, WindowEvent},
 };
+
 pub struct CooperApplication {
     window: Window,
     renderer: Box<dyn Renderer>,
     event_handler: EventHandler
 }
-
 impl CooperApplication {
-    pub fn create() -> Self {
+    pub fn create() -> CooperApplication {
         let window = Window::create("Cooper", 1280., 720.);
         
         let renderer = Box::new(VulkanRenderer::create(&window));
@@ -77,32 +79,33 @@ impl CooperApplication {
             event_handler
         }
     }
-
-    pub fn run(mut self) -> () {
-        let mut cursor_position  = None;
+    pub fn run(mut self) -> (){
+        let mut cursor_position = None;
         let mut wheel_delta = None;
-
         let event_loop = self.window.event_loop;
-        
-
         event_loop.run( move
-            |event, elwt|{
+            |event, _elwt|{
                 match event{
+                    
                     Event::WindowEvent {event, .. } => match event {
                         WindowEvent::RedrawRequested=> {
                             println!("Redraw Requested");
                             self.renderer.draw_frame();
                         }
                         WindowEvent::CloseRequested => {return},
-                        WindowEvent::Resized(PhysicalSize { width, height }) => {
+                        WindowEvent::Resized(PhysicalSize { width: _, height: _ }) => {
                             //resize_dimensions = Some([width as u32, height as u32]);
                         }
                         WindowEvent::MouseInput {
                             button: MouseButton::Left,
-                            state,
+                            state: _,
                             ..
                         } => {
-
+                            //if state == ElementState::Pressed {
+                            //    is_left_clicked = Some(true);
+                            //} else {
+                            //    is_left_clicked = Some(false);
+                            //}
                         }
                         WindowEvent::CursorMoved { position, .. } => {
                             let position: (i32, i32) = position.into();

bdd1c0ebffac8a7e97be05a25c73d01efecad05a
Aidan Dowling <me@aodhain.ie>
2023-10-04 01:19:09 +0100

    basic movement

--- crates/lynch/src/camera.rs
+++ crates/lynch/src/camera.rs
@@ -19,7 +19,39 @@ impl Camera {
     pub fn position(&self) -> Point3<f32> {
         self.location
     }
+    pub fn get_forward(&self) -> [f32; 3]{
+        let forward_vector = [
+            self.pitch.cos() * self.yaw.sin(),
+            self.pitch.sin(),
+            self.pitch.cos() * self.yaw.cos()
+        ];
+        return normalize(forward_vector);
+    } 
+    pub fn move_forward(&mut self, amount: f32){
+
+        let forward = self.get_forward();
+        self.location[0] +=  forward[0] * amount;
+        self.location[1] +=  forward[1] * amount;
+        self.location[2] +=  forward[2] * amount;
+    }
+    pub fn move_sideways(&mut self, amount: f32){
+        let forward = self.get_forward();
+
+        let sideway = cross_product(forward, [0.,1.,0.]);
+
+        self.location[0] +=  sideway[0] * amount;
+        self.location[1] +=  sideway[1] * amount;
+        self.location[2] +=  sideway[2] * amount;
+    }
 }
+
+//orbital
+//Point3::new(
+//     self.r * self.phi.sin() * self.theta.sin(),
+//     self.r * self.phi.cos(),
+//     self.r * self.phi.sin() * self.theta.cos(),
+// )
+
 impl Default for Camera {
     fn default() -> Self {
         Camera {

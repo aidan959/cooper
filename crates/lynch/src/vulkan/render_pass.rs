dc9af745b6dcf9562b0c9073357714311f78a757
Aidan Dowling <me@aodhain.ie>
2023-10-13 12:47:06 +0100

    render pass

--- crates/lynch/src/vulkan/render_pass.rs
+++ crates/lynch/src/vulkan/render_pass.rs
@@ -1,4 +1,29 @@
 use ash::vk;
 
 
-pub
+pub struct RenderPass {
+    pub name: String,
+    pub pipeline_handle: PipelineId,
+    pub render_func: Option<
+        Box<dyn Fn(&Device, &vk::CommandBuffer, &VulkanRenderer, &RenderPass, &GraphResources)>,
+    >,
+    pub reads: Vec<Resource>,
+
+    pub writes: Vec<Attachment>,
+}
+impl RenderPass {
+    pub fn new(
+        name: String,
+        pipeline_handle: PipelineId,
+        render_func: Option<
+            Box<dyn Fn(&Device, &vk::CommandBuffer, &VulkanRenderer, &RenderPass, &GraphResources)>,
+        >,
+    ) -> RenderPass {
+        RenderPass {
+            name,
+            pipeline_handle,
+            render_func,
+            reads: Vec::new(),
+            writes: Vec::new(),
+        }
+    }

0dc75c5adbb90ca86e4ec728485b2f68e2b10427
Aidan Dowling <me@aodhain.ie>
2023-10-13 12:37:36 +0100

    descriptor set creation

--- crates/lynch/src/vulkan/descriptor.rs
+++ crates/lynch/src/vulkan/descriptor.rs
@@ -26,6 +26,50 @@ impl DescriptorSet {
         layout: vk::DescriptorSetLayout,
         binding_map: BindingMap,
     ) -> DescriptorSet {
+        let descriptor_pool_sizes = binding_map
+            .values()
+            .map(|val| {
+                let descriptor_type = match val.info.ty {
+                    rspirv_reflect::DescriptorType::COMBINED_IMAGE_SAMPLER => {
+                        vk::DescriptorType::COMBINED_IMAGE_SAMPLER
+                    }
+                    rspirv_reflect::DescriptorType::SAMPLED_IMAGE => {
+                        vk::DescriptorType::SAMPLED_IMAGE
+                    }
+                    rspirv_reflect::DescriptorType::STORAGE_IMAGE => {
+                        vk::DescriptorType::STORAGE_IMAGE
+                    }
+                    rspirv_reflect::DescriptorType::UNIFORM_BUFFER => {
+                        vk::DescriptorType::UNIFORM_BUFFER
+                    }
+                    rspirv_reflect::DescriptorType::STORAGE_BUFFER => {
+                        vk::DescriptorType::STORAGE_BUFFER
+                    }
+                    _ => unimplemented!(),
+                };
+
+                vk::DescriptorPoolSize::builder()
+                    .ty(descriptor_type)
+                    .descriptor_count(1)
+                    .build()
+            })
+            .collect::<Vec<_>>();
+        let descriptor_pool = {
+            let descriptor_pool_info = vk::DescriptorPoolCreateInfo::builder()
+                .pool_sizes(&descriptor_pool_sizes)
+                .flags(
+                    vk::DescriptorPoolCreateFlags::FREE_DESCRIPTOR_SET
+                        | vk::DescriptorPoolCreateFlags::UPDATE_AFTER_BIND,
+                )
+                .max_sets(descriptor_pool_sizes.len() as u32);
+
+            unsafe {
+                device
+                    .device()
+                    .create_descriptor_pool(&descriptor_pool_info, None)
+                    .expect("Error creating descriptor pool")
+            }
+        };
         todo!()
     }
 

9a3892ff1da0d247cbeb9d3bd2bc58e1783bebfb
Aidan Dowling <me@aodhain.ie>
2023-12-05 18:08:23 +0000

    project interface fixed

--- crates/lynch/src/vulkan/mod.rs
+++ crates/lynch/src/vulkan/mod.rs
@@ -1,11 +1,21 @@
-pub mod renderer;
+mod buffer;
+mod cont;
+mod debug;
+mod descriptor;
+mod device;
+mod image;
+mod pipeline;
+mod render_pass;
+pub mod shader;
 mod surface;
 mod swapchain;
-mod debug;
-mod cont;
-mod render_pass;
-mod buffer;
-mod descriptor;
-mod shader;
-mod texture;
-mod mesh_loader;
+
+pub mod renderer;
+pub use buffer::Buffer;
+pub use descriptor::{DescriptorIdentifier, DescriptorSet};
+pub use device::Device;
+pub use image::{Image, ImageCopyDescBuilder, ImageDesc, ImageType};
+pub use pipeline::{Pipeline, PipelineDesc, PipelineDescBuilder, PipelineType};
+pub use render_pass::RenderPass;
+
+pub use device::{global_pipeline_barrier, image_pipeline_barrier};

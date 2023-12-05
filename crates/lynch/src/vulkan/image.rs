99b0342f4526a226eeb92c353871ecb9e683939b
Aidan Dowling <me@aodhain.ie>
2023-12-05 18:18:50 +0000

    updated structure for image storage

--- crates/lynch/src/vulkan/image.rs
+++ crates/lynch/src/vulkan/image.rs
@@ -56,7 +56,11 @@ impl ImageDesc {
 pub struct Image {
     pub image: vk::Image,
     pub image_view: vk::ImageView,
+    pub layer_views: Vec<vk::ImageView>,
+    pub device_memory: vk::DeviceMemory,
+    pub current_layout: vk::ImageLayout,
     pub desc: ImageDesc,
+    pub debug_name: String,
     pub device: Arc<Device>,
 }
 
@@ -69,6 +73,113 @@ impl Image {
                 .ash_device
                 .destroy_image_view(self.image_view, None);
             self.device.ash_device.destroy_image(self.image, None);
+        }
+    }
+    pub fn new_from_desc(device: Arc<Device>, desc: ImageDesc) -> Image {
+        unsafe {
+            let initial_layout = vk::ImageLayout::UNDEFINED;
+            let image_create_info = vk::ImageCreateInfo {
+                image_type: vk::ImageType::TYPE_2D,
+                format: desc.format,
+                extent: vk::Extent3D {
+                    width: desc.width,
+                    height: desc.height,
+                    depth: 1,
+                },
+                mip_levels: desc.mip_levels,
+                array_layers: desc.array_layers,
+                samples: vk::SampleCountFlags::TYPE_1,
+                tiling: vk::ImageTiling::OPTIMAL,
+                usage: desc.usage,
+                sharing_mode: vk::SharingMode::EXCLUSIVE,
+                initial_layout,
+                flags: if desc.image_type == ImageType::Cube
+                    || desc.image_type == ImageType::CubeArray
+                {
+                    vk::ImageCreateFlags::CUBE_COMPATIBLE
+                } else {
+                    vk::ImageCreateFlags::empty()
+                },
+                ..Default::default()
+            };
+            let image = device
+                .ash_device
+                .create_image(&image_create_info, None)
+                .expect("Unable to create image");
+
+            // Memory allocation
+            let image_memory_req = device.ash_device.get_image_memory_requirements(image);
+            let image_memory_index = device
+                .find_memory_type_index(&image_memory_req, vk::MemoryPropertyFlags::DEVICE_LOCAL)
+                .expect("Unable to find suitable memory index for image");
+            let image_allocate_info = vk::MemoryAllocateInfo {
+                allocation_size: image_memory_req.size,
+                memory_type_index: image_memory_index,
+                ..Default::default()
+            };
+            let device_memory = device
+                .ash_device
+                .allocate_memory(&image_allocate_info, None)
+                .expect("Unable to allocate image device memory");
+
+            device
+                .ash_device
+                .bind_image_memory(image, device_memory, 0)
+                .expect("Unable to bind device memory to image");
+
+            let view_type = if desc.image_type == ImageType::Tex2d && desc.array_layers == 1 {
+                vk::ImageViewType::TYPE_2D
+            } else if desc.image_type == ImageType::Tex2dArray && desc.array_layers > 1 {
+                vk::ImageViewType::TYPE_2D_ARRAY
+            } else if desc.image_type == ImageType::Cube {
+                vk::ImageViewType::CUBE
+            } else {
+                unimplemented!()
+            };
+
+            let image_view = Image::create_image_view(
+                &device,
+                image,
+                desc.format,
+                desc.aspect_flags,
+                view_type,
+                0,
+                desc.array_layers,
+                desc.mip_levels,
+            );
+
+            let mut layer_views = vec![];
+
+            if desc.array_layers > 1 {
+                for layer in 0..desc.array_layers {
+                    let view = Image::create_image_view(
+                        &device,
+                        image,
+                        desc.format,
+                        desc.aspect_flags,
+                        if desc.image_type == ImageType::Cube {
+                            vk::ImageViewType::TYPE_2D
+                        } else {
+                            view_type
+                        },
+                        layer,
+                        1,
+                        desc.mip_levels,
+                    );
+                    layer_views.push(view);
+                }
+            }
+
+            Image {
+                image,
+                image_view,
+                layer_views,
+                device_memory,
+                current_layout: initial_layout,
+                desc,
+                debug_name: "unnamed_image".to_string(),
+                device,
+            }
         }
     }
 }

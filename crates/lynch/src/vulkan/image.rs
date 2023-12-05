00409dae5b64e1280c1871a81ae4b214b818b002
Aidan Dowling <me@aodhain.ie>
2023-12-05 18:08:30 +0000

    creating image container

---
+++ crates/lynch/src/vulkan/image.rs
@@ -0,0 +1,30 @@
+use std::sync::Arc;
+
+use ash::vk;
+use ash::vk::{AccessFlags, ImageLayout, PipelineStageFlags};
+
+use super::Device;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum ImageType {
+    Tex1d = 0,
+    Tex1dArray = 1,
+    Tex2d = 2,
+    Tex2dArray = 3,
+    Tex3d = 4,
+    Cube = 5,
+    CubeArray = 6,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct ImageDesc {
+    pub width: u32,
+    pub height: u32,
+    pub depth: u32,
+    pub array_layers: u32,
+    pub format: vk::Format,
+    pub image_type: ImageType,
+    pub aspect_flags: vk::ImageAspectFlags,
+    pub usage: vk::ImageUsageFlags,
+    pub mip_levels: u32,
+}

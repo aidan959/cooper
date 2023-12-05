a8ae16c3cf73b32d2b3119fbaaa712a72504022d
Aidan Dowling <me@aodhain.ie>
2023-12-05 18:20:34 +0000

    Updated mod.rs for lynch

--- crates/lynch/src/vulkan/mod.rs
+++ crates/lynch/src/vulkan/mod.rs
@@ -1,21 +1,22 @@
-mod buffer;
 mod cont;
 mod debug;
-mod descriptor;
+mod swapchain;
+mod buffer;
+mod surface;
 mod device;
 mod image;
+mod descriptor;
+pub mod shader;
 mod pipeline;
 mod render_pass;
-pub mod shader;
-mod surface;
-mod swapchain;
 
 pub mod renderer;
-pub use buffer::Buffer;
-pub use descriptor::{DescriptorIdentifier, DescriptorSet};
 pub use device::Device;
-pub use image::{Image, ImageCopyDescBuilder, ImageDesc, ImageType};
-pub use pipeline::{Pipeline, PipelineDesc, PipelineDescBuilder, PipelineType};
-pub use render_pass::RenderPass;
+pub use image::{Image, ImageDesc, ImageType,ImageCopyDescBuilder};
+pub use buffer::Buffer;
+pub use  pipeline::{Pipeline,PipelineDesc,PipelineDescBuilder,PipelineType};
+pub use descriptor::{DescriptorIdentifier, DescriptorSet};
+pub use  render_pass::RenderPass;
+
 
 pub use device::{global_pipeline_barrier, image_pipeline_barrier};

9411bfff23b2149de34dcd0093e98b2e8bb67808
Aidan Dowling <me@aodhain.ie>
2024-01-18 14:15:29 +0000

    started ecs

--- crates/frost/src/lib.rs
+++ crates/frost/src/lib.rs
@@ -3,3 +3,19 @@ pub mod world;
 mod input;
 
 pub use input::Input; 
+
+pub(crate) type EntityId = u32;
+pub(crate) type Generation = EntityId;
+pub(crate) type PackId = u64;
+
+#[derive(Clone, Copy)]
+pub(crate) struct EntityMeta {
+    pub(crate) generation: Generation,
+    pub(crate) location: EntityLocation,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct EntityLocation {
+    archetype_index: EntityId,
+    index_in_archetype: EntityId,
+}

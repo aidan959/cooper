86ad1920f0f69c12f40bfbf6f8bb1d1236c9c433
Aidan Dowling <me@aodhain.ie>
2024-01-24 18:10:39 +0000

    cool mutable retrieval code :) Tricking the borrow checker 💪

---
+++ crates/frost/src/util.rs
@@ -0,0 +1,13 @@
+pub fn retrieve_two_mutable<T>(slice: &mut [T], first: usize, second: usize) -> (&mut T, &mut T) {
+    if first < second {
+        let (a, b) = slice.split_at_mut(second);
+        let f = &mut a[first];
+        let s = &mut b[0];
+        (f, s)
+    } else {
+        let (a, b) = slice.split_at_mut(first);
+        let f = &mut b[0];
+        let  s = &mut a[second];
+        (f, s)
+    }
+}
